6fb64b6356ef993c6945386c63720c03a3fe8fb5
AgomohC <chinaemerem98@yahoo.com>
2021-10-31 21:45:26 +0100

    added readme.md

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+//  require and initialize dependencies
 const express = require("express");
 const app = express();
 require("dotenv").config();
-app.use(express.static("./public"));
 const multer = require("multer");
 const DataUriParser = require("datauri/parser");
 const path = require("path");
@@ -9,36 +9,47 @@ const { cloudinaryConfig, uploader } = require("./config/cloudinaryConfig");
 const parser = new DataUriParser();
 const storage = multer.memoryStorage();
 const multerUploads = multer({ storage }).single("upfile");
+
+// middleware
+app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("*", cloudinaryConfig);
+
+// routes
 app.post("/api/fileanalyse", multerUploads, async (req, res) => {
   try {
+    //  check if file to upload exists
     const upfile = req.file;
     if (!upfile) {
       return res.status(400).json({ msg: "file not found" });
     }
-
+    //  upload file
     const upload = await uploader.upload(
       parser.format(
         path.extname(req.file.originalname).toString(),
         req.file.buffer
       ).content
     );
-    console.log(upload);
-    res.status(201).json({
+
+    //  json return
+    return res.status(201).json({
       name: upfile.originalname,
       type: upfile.mimetype,
       size: upfile.size,
-      img: upload.url,
+      img: upload.secure_url,
     });
   } catch (error) {
-    res.status(500).send({ msg: error });
+    return res.status(500).send({ msg: error });
   }
 });
+
+// not found handler
 app.use((req, res) => {
   res.status(404).send("route does not exist");
 });
+
+// listener function
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`app is listening on port ${port}...`);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# File Metadata
+
+[femto-metadata](#) is a simple full stack app that accepts a file and returns its metadata. This project idea was gotten from [freeCodeCamp]()
